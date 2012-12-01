12d02ad6cc4ec20493e44f4f16dfc7ccf6051036
ndalama <ndalamak@gmail.com>
2012-12-01 14:25:31 +0200

    Added stationdetail

--- src/com/powermonitor/Location.java
+++ src/com/powermonitor/Location.java
@@ -3,6 +3,7 @@ package com.powermonitor;
 public class Location {
 	public long Latitude;
 	public long Longitude;
+	public String name;
 	
 	public Location() {
 		this.Latitude = (long) 0.0;
---
+++ src/com/powermonitor/StationDetailActivity.java
@@ -0,0 +1,37 @@
+package com.powermonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class StationDetailActivity extends Activity {
+	
+	public static Station station = null;
+	
+	public static void SetStation(Station my_station)
+	{
+		station = my_station; 
+	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+
+		TextView txtStationName = (TextView) findViewById(R.id.txtStationName);
+		TextView txtStationLocation = (TextView) findViewById(R.id.txtLocation);
+		
+		txtStationName.setText(station.name);
+		txtStationLocation.setText(station.location.name);
+		
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    
+}
